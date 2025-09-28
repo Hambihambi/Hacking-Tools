16a636e55a69668f751d61e154ad0576e3afd51d
Hambihambi <134943206+Hambihambi@users.noreply.github.com>
2025-09-28 18:07:03 +0300

    Update sniff_packet.py
    Added a HTTP Request based sniffing filter to the main function.

--- packet_sniffer/sniff_packet.py
+++ packet_sniffer/sniff_packet.py
@@ -1,2 +1,12 @@
 #!/usr/bin/env python
 import scapy.all as scapy
+from scapy.layers import http
+
+def sniff(interface):
+    scapy.sniff(iface=interface, store=False, prn=packet_processer)
+
+def packet_processer(packet):
+    if packet.haslayer(http.HTTPRequest):
+        print(packet)
+
+sniff("eth0")
