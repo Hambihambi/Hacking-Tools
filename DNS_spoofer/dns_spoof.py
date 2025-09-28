99398d85db4cdf28ab8b283f312803f238ce0c88
Hambihambi <134943206+Hambihambi@users.noreply.github.com>
2025-09-29 01:03:39 +0300

    Update dns_spoof.py

--- DNS_spoofer/dns_spoof.py
+++ DNS_spoofer/dns_spoof.py
@@ -1,3 +1,13 @@
 #!/usr/bin/env python3
 
-import scapy.all as scapy
+import netfilterqueue
+
+def processpacket(packet):
+    print(packet)
+    # .drop or .access the packet
+    packet.accept()
+
+# create the object, bind to the iptables command and run
+queue = netfilterqueue.NetfilterQueue()
+queue.bind(1337, process_packet)
+queue.run()
