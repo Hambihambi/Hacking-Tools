1136f6b9d2a5d0eee081c64654107b32e488d3e6
Hambihambi <134943206+Hambihambi@users.noreply.github.com>
2025-09-30 23:30:24 +0300

    Update network_scan.py
    Added more functionality, more verbosity, and cutting edge technology

--- network_scanner/network_scan.py
+++ network_scanner/network_scan.py
@@ -1,35 +1,53 @@
 #!/usr/bin/env python3
-import scapy.all as scapy
+from scapy.layers.l2 import ARP, Ether
+from scapy.sendrecv import srp
 import argparse
+import sys
+import os
 
+def root_check():
+    if os.geteuid() != 0:
+        sys.exit("[!] This script must run as root")
+    else:
+        print("[*] Welcome to the network scanner.")
 
 def get_args():
-    parser = argparse.ArgumentParser(description='Scan the network for MAC addresses by IP range', epilog='e.g.: networkscanner -i 192.168.0.1/24')
-    parser.add_argument("-i", "--ip_address", dest="ip_address", required=True, help="the ip address to scan, e.g.: 192.168.0.1/24")
-    args = parser.parse_args()
-    return args
+    parser = argparse.ArgumentParser(
+        description='Scan the network for MAC addresses by IP range',
+        epilog='e.g.: networkscanner -i 192.168.56.1/24'
+    )
+    parser.add_argument(
+        "-i", "--ip_range",
+        dest="ip_range",
+        required=True,
+        help="the ip address to scan, e.g.: 192.168.0.1/24"
+    )
+    return parser.parse_args()
 
+def scan(ip_range):
+    try:
+        print(f"[*] Creating ARP request for IP range: {ip_range}")
+        arp_request = ARP(pdst=ip_range)
+        broadcast = Ether(dst="ff:ff:ff:ff:ff:ff")
+        packet = broadcast/arp_request
+        print("[*] Sending broadcast ARP request...")
+        answered_list = srp(packet, timeout=2, retry=1, verbose=False)[0]
+        print(f"[*] Received {len(answered_list)} responses")
+    except Exception as e:
+        sys.exit(f"[!] Error scanning the network: {e}")
 
-def scan(ip):
-    arp_request = scapy.ARP(pdst=ip)
-    broadcast = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
-    arp_request_broadcast = broadcast/arp_request
-    answered_list = scapy.srp(arp_request_broadcast, timeout=1, verbose=False)[0]
-    client_list = []
+    return [{"ip": res[1].psrc, "mac": res[1].hwsrc} for res in answered_list]
 
-    for element in answered_list:
-        client_dict = {"ip": element[1].psrc, "mac": element[1].hwsrc}
-        client_list.append(client_dict)
+def print_result(clients):
+    print("[+] Scan Results:")
+    print("[+] IP address".ljust(20, ".") + "MAC address")
+    for client in clients:
+        print(client["ip"].ljust(20) + client["mac"])
 
-    return client_list
-
-
-def print_result(result_list):
-    print("IP address" + "\t\t" + "MAC address" + "\n")
-    for client in result_list:
-        print(client["ip"] + "\t\t" + client["mac"])
-
-
-arguments = get_args()
-scan_result = scan(arguments.ip_address)
-print_result(scan_result)
+if __name__ == "__main__":
+    root_check()
+    args = get_args()
+    print(f"[*] Starting scan on: {args.ip_range}")
+    result = scan(args.ip_range)
+    print_result(result)
+    print("[*] Network scan completed.")
